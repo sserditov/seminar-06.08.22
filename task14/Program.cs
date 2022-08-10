40fadf88f61b9a1efd85e51ab89766264e9be7ce
sserditov <mozoz@yandex.ru>
2022-08-10 18:30:34 +0500

    Add Task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 14: Напишите программу, которая принимает на вход число и проверяет,
+//кратно ли оно одновременно 7 и 23.
+//14 -> нет
+//46 -> нет
+//161 -> да
+Console.WriteLine("Введите число");
+int namber = Convert.ToInt32(Console.ReadLine());
+if (namber % 7 == 0 | namber % 23 == 0)
+{
+    Console.WriteLine("да, кратно");
+}
+else
+{
+    Console.WriteLine("нет, не кратно");
+}
