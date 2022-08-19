ab1912af37592e24e731866f3481423d7de08324
sserditov <mozoz@yandex.ru>
2022-08-19 17:28:17 +0500

    Add Task22_1

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -5,3 +5,9 @@
 
 Console.WriteLine("Введите число А");
 int A = Convert.ToInt32(Console.RedLine());
+int sum = 0;
+for (int i = 1; i <= A; i++) ;
+{
+    sum = sum + i; /* sum +=i */
+}
+Console.WriteLine($ "Сумма чиселот 1 до А");
