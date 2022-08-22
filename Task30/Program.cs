78816c669662acb806fef0504854d7a4178c4450
sserditov <mozoz@yandex.ru>
2022-08-22 17:19:16 +0500

    Alternative

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -29,3 +29,14 @@ int count = array.Length;
 
 FillArray(array);
 PrintArray(array);
+
+/* альтернативное решение
+Console.WriteLine(string.Join(", ", array));
+int[] arr new int[8];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++) ;
+{
+    arr[i] = rand.Next(0, 2);
+}
+
+Console.WriteLine(string.Join(", ", arr)); */
