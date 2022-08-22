84e28661606a5cda478b49a8a504092b3d316cd7
sserditov <mozoz@yandex.ru>
2022-08-22 18:58:53 +0500

    New

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,10 +1,23 @@
 ﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов
  и выводит их на экран.
- Вводим массив через консоль */
+ Вводим массив через консоль 
 int[] mas = { 8, 6, 3, 9, 11, 13, 7, 15 };
 void function(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
 }
-function(mas);
+function(mas); */
+
+int[] array = new int[8];
+
+Console.WriteLine("Введите числа для массива");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("массив");
+Console.Write(string.Join(", ", array));
+
+
